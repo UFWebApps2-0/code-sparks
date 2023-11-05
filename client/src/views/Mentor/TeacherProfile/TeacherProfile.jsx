ae319beea5c0255e7a479f7d37436f50b6f39155
KJ O'BRIEN <kjokjo219@gmail.com>
2023-11-05 10:41:05 -0500

    Techer profile with default image

--- client/src/views/Mentor/TeacherProfile/TeacherProfile.jsx
+++ client/src/views/Mentor/TeacherProfile/TeacherProfile.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import NavBar from "../../../components/NavBar/NavBar";
 import './TeacherProfile.less';
+import Logo from '../../../assets/default.png';
 
 //container for teacher profile, no functionality currently - placeholder div
 export default function TeacherProfile(props) {
   return (
     <div className="container nav-padding">
       <NavBar />
-      <div>Teacher Profile</div> 
+      <div>
+      <img class="profile_picture_styling"src={Logo} alt="Profile Picture" />
+      </div> 
+      
+    <div>
+   
     </div>
+    </div>
+    
   )
 }
