6695e05542b0fa76878d59432f8a684370635e34
KJ O'BRIEN <kjokjo219@gmail.com>
2023-11-05 11:04:24 -0500

    Adds divs that will be used as buttons

--- client/src/views/Mentor/TeacherProfile/TeacherProfile.jsx
+++ client/src/views/Mentor/TeacherProfile/TeacherProfile.jsx
@@ -3,18 +3,22 @@ import NavBar from "../../../components/NavBar/NavBar";
 import './TeacherProfile.less';
 import Logo from '../../../assets/default.png';
 
-//container for teacher profile, no functionality currently - placeholder div
 export default function TeacherProfile(props) {
   return (
     <div className="container nav-padding">
       <NavBar />
       <div>
-      <img class="profile_picture_styling"src={Logo} alt="Profile Picture" />
-      </div> 
-      
-    <div>
-   
-    </div>
+        <div class = "teacher_styling" >
+          <img class="profile_picture_styling" src={Logo} alt="Profile Picture" />
+        </div> 
+        <div class = " teacher_styling teacher_buttons">
+          <h1>Challenge View</h1>
+          </div>
+        <div class = "teacher_styling teacher_buttons">
+         <h1>Create New Assignment</h1>
+        </div>
+      </div>
+
     </div>
     
   )
