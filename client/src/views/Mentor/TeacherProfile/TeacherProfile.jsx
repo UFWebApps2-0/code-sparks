c623ac2398c4d26bd7d698e122d8eddf6ca0627a
KJ O'BRIEN <kjokjo219@gmail.com>
2023-11-06 12:38:45 -0500

    sprint 1

--- client/src/views/Mentor/TeacherProfile/TeacherProfile.jsx
+++ client/src/views/Mentor/TeacherProfile/TeacherProfile.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import NavBar from "../../../components/NavBar/NavBar";
 import './TeacherProfile.less';
-//import Logo from '../../../assets/default.png';
+import default_profile from '../../../assets/default.png';
 //import { getToken} from '../../../Utils/AuthRequests';
 //import { getCurrUser} from '../../../Utils/userState';
 import { getMentor} from '../../../Utils/requests';
 
 //import { getMentorProfilePicture} from '../../../Utils/requests';
 
+let teacher_profile_picture = default_profile;
 let teacher_profile = await getMentor();
-//console.log(x);
-//console.log(x.data.profile_picture);
+
+
 
 export default function TeacherProfile(props) {
+  //try catch at some point?
+  teacher_profile_picture = teacher_profile.data.profile_picture;
+  
 
   return (
     <div className="container nav-padding">
       <NavBar />
       <div>
         <div class = "teacher_styling" >
-          <img class="profile_picture_styling" src={teacher_profile.data.profile_picture} alt="Profile Picture" />
+          <img class="profile_picture_styling" src={teacher_profile_picture} alt="Profile Picture" />
         </div>       
         <div class = " teacher_styling teacher_buttons">
           <h1>Challenge View</h1>
