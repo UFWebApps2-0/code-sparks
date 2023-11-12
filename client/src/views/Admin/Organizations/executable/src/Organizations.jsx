96710951d54ae97397c64e3f448fcecfbe35dac0
medwards14 <medwards1@ufl.edu>
2023-11-11 23:33:28 -0500

    created the organizations page and got it working

---
+++ client/src/views/Admin/Organizations/executable/src/Organizations.jsx
@@ -0,0 +1,48 @@
+/* this is my .jsx file for the classrooms page; it runs successfully if you change directory to ./Sapphire-Project12-4f\client\src\views\Admin\Classrooms\executable then type npm run dev into the terminal. */
+import './Organizations.css';
+import organizations from './organizationsData.json'
+
+function Organizations() {
+  
+  return(
+    <div>
+      <h1 className="organizations-header">Organizations</h1>
+      <div className="App">
+      
+      {
+        organizations && organizations.map( org => {
+          return(
+             <div className="box" key={ org.id }>
+              <strong>{ org.orgName }</strong> <br />
+              <div>County: { org.county } </div>
+              <div>State: { org.state }</div>
+              <div>Number of Classrooms: { org.classrooms.length }</div>
+               {org.classrooms.map( data => {
+                return( 
+                  <div key={org.key}>
+                  { data.classroomName } 
+                  </div>
+                )
+               })}
+               <div>Number of Mentors: { org.mentors.length }</div>
+               {org.mentors.map( data => {
+                return( 
+                  <div key={org.key}>
+                  { data.firstName } { data.lastName } 
+                  </div>
+                )
+               })}
+               <br />
+               
+             </div>
+          )
+        })
+      }
+
+      </div>
+    </div>
+  );
+}
+
+export default Organizations;
+
