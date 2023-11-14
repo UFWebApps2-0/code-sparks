6a75fe6e7c6218f5d3c75b0763031fedfe8c2945
medwards14 <medwards482@gmail.com>
2023-11-13 20:36:22 -0500

    updated comments on my Organizations.jsx file

--- client/src/views/Admin/Organizations/executable/src/Organizations.jsx
+++ client/src/views/Admin/Organizations/executable/src/Organizations.jsx
@@ -1,9 +1,14 @@
-/* this is my .jsx file for the classrooms page; it runs successfully if you change directory to ./Sapphire-Project12-4f\client\src\views\Admin\Classrooms\executable then type npm run dev into the terminal. */
+/* this is my (mason edwards) .jsx file i developed on my own for the Organizations list page, a lot of which I reused from my Classrooms.jsx file. I did not give anyone permission to directly copy from 
+my code at any point and had this all done before 11/13. */
 import './Organizations.css';
+/* here I imported my mock data (which is identical to the data on strapi, but still created by me in a json file) for organizations into the organizations variable for later use */
 import organizations from './organizationsData.json'
 
 function Organizations() {
-  
+  /* in this file I used a total of three maps to first iterate through each organization, then separately iterate through its array of classrooms, then its array of mentors. The first element i output is 
+  the 'Organizations' header, then for each organization I display its name, county, and state. Then i use the second map to store each organizations array of classrooms into the 'data' variable and display 
+  each one. I do the same thing for the array of mentors as well. I used <strong></strong>, <div>/<div>, and <br > to control the spacing. Also, I made adjustments to the index.html file to center the
+  display and change the font. */
   return(
     <div>
       <h1 className="organizations-header">Organizations</h1>
