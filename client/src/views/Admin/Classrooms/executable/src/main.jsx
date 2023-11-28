1258f71bbe2b1b6a7da2289ae8eae141e6fdd663
medwards14 <medwards1@ufl.edu>
2023-11-27 21:03:44 -0500

    Got Strapi Data implemented in Classrooms List

--- client/src/views/Admin/Classrooms/executable/src/main.jsx
+++ client/src/views/Admin/Classrooms/executable/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import Classrooms from './Classrooms.jsx'
 import './index.css'
 import './Classrooms.css'
-import './classroomsData.json'
 
 /* this is my main.jsx file for the classrooms list page, where I import my Classrooms.css file which has the blue color settings, as well as my classroomsData.json file, which has my mock data.
 this file was originally auto-generated when I did npm create vite@latest, so the only other change I made was adding the Classrooms reference below to the Classrooms.jsx file I created. */
