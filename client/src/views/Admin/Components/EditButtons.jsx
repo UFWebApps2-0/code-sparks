fec1b7bf66fcdb1c4e91946843d3fc31ef35e702
KyleHoNguyen <kyle.honguyen@ufl.edu>
2023-12-06 15:27:43 -0500

    Formatting main page better

--- client/src/views/Admin/Components/EditButtons.jsx
+++ client/src/views/Admin/Components/EditButtons.jsx
@@ -31,7 +31,8 @@ const EditButtons = () => {
             <button className="first-button" onClick={routeOrganizations}>Edit Organizations</button>
             <button className="custom-button" onClick={routeClassrooms}>Edit Classrooms</button>
             <button className="custom-button" onClick={routeTeachers}>Edit Teachers</button>
-            <button className="custom-button" onClick={routeLessons}>Edit Lessons</button>
+            <br/>
+            <button className="first-button" onClick={routeLessons}>Edit Lessons</button>
             <button className="custom-button" onClick={routeManageStudents}>Manage Students</button>
             <button className="custom-button" onClick={routeWorkspaces}>View Workspaces</button>
         </div>
